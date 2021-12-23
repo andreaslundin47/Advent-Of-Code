336d9b24fb301dbb576f5a1aafa065f3600f9e8f
Andreas Lundin <andreaslundin47@gmail.com>
2021-12-23 23:50:30 +0100

    Improved Day 22 based on solution by William Y. Feng.

--- day_22/day_22.py
+++ day_22/day_22.py
@@ -14,15 +14,10 @@ with open('input', 'r') as f :
 
 
 def overlaps(c1, c2) :
-    for a1, a2 in zip(c1, c2) :
-        if a1[1] < a2[0] or a1[0] > a2[1] :
-            return False
-    return True
-
-
-def get_overlap_bounds(c1, c2) :
     bounds = []
     for a1, a2 in zip(c1, c2) :
+        if a1[1] < a2[0] or a1[0] > a2[1] :
+            return None
         bounds.append( (max(a1[0], a2[0]), min(a1[1], a2[1])) )
     return tuple(bounds)
 
@@ -35,30 +30,31 @@ def volume(bounds) :
 
 
 """
-    Finds the total number of lights via a naive implementation of 
-    the set inclusion and exclusion principle.
+    Finds the total number of lights using the set inclusion exclusion 
+    principle. Inspired by William Y. Feng.
 """
-def calculate_lights(cubes) :
-    boxes = defaultdict(int)
-    unique_id = 0
-    for mode, bounds in cubes :
-        next_boxes = defaultdict(int)
-        for (box, _), contribution in boxes.items() :
-            if overlaps(bounds, box) :
-                overlap = get_overlap_bounds(bounds, box)
-                op_sign = -1 if contribution > 0 else 1
-                next_boxes[(overlap, unique_id)] += op_sign * volume(overlap)
-                unique_id += 1
-            next_boxes[(box, unique_id)] += contribution
-            unique_id += 1
-        if mode == 'on' :
-            next_boxes[(bounds, unique_id)] += volume(bounds)
-        boxes = {b:c for b,c in next_boxes.items() if c != 0}
-    return sum(boxes.values())
+def count_lights(cubes) :
+    regions_and_contributions = defaultdict(int)
+
+    for setting, new_cube_bounds in cubes :
+        new_overlaps = defaultdict(int)
+
+        for existing_region in regions_and_contributions.keys() :
+            new_overlap = overlaps(new_cube_bounds, existing_region)
+            if new_overlap :
+                new_overlaps[new_overlap] -= regions_and_contributions[existing_region]
+
+        if setting == 'on' :
+            new_overlaps[new_cube_bounds] += 1
+
+        for box, counts in new_overlaps.items() :
+            regions_and_contributions[box] += counts
+
+    return sum(volume(b) * count for b, count in regions_and_contributions.items())
 
 
 bounds_505050 = ( (-50,50), (-50,50), (-50,50) )
 center_cubes = filter(lambda c: overlaps(c[1], bounds_505050), cubes)
 
-print(f"Part 1. Sum: {calculate_lights(center_cubes)}")
-print(f"Part 2. Sum: {calculate_lights(cubes)}")
+print(f"Part 1. Sum: {count_lights(center_cubes)}")
+print(f"Part 2. Sum: {count_lights(cubes)}")
