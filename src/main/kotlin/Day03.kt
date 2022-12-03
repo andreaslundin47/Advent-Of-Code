ee11afeeb14dea884836d2cfcdf8c52a649b01f4
Andreas Lundin <andreaslundin47@gmail.com>
2022-12-03 19:07:55 +0100

    Simplified day 3 Kotlin a bit

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -5,22 +5,21 @@ val rucksacks: List<String> = File("src/main/resources/day03.txt").readLines()
 fun Char.itemPriority(): Int = if (this.isUpperCase()) (this - 'A' + 27) else (this - 'a' + 1)
 
 fun solvePartOne() {
-    val prioritySum = rucksacks.sumOf { rucksack ->
+    val prioritySum: Int = rucksacks.sumOf { rucksack ->
         val compSize = rucksack.length / 2
-        val firstCompartment = rucksack.take(compSize).toCharArray().toSet()
-        val secondCompartment = rucksack.takeLast(compSize).toCharArray().toSet()
-        val intersection = firstCompartment.intersect(secondCompartment)
+        val firstCompartment = rucksack.take(compSize).toSet()
+        val secondCompartment = rucksack.takeLast(compSize).toSet()
+        val sharedItem: Char = firstCompartment.intersect(secondCompartment).first()
 
-        intersection.first().itemPriority()
+        sharedItem.itemPriority()
     }
     println("Part 1. Sum = $prioritySum")
 }
 
 fun solvePartTwo() {
-    val elfGroups = rucksacks.chunked(3)
-    val prioritySum = elfGroups.sumOf { group ->
-        val rucksackSets = group.map { rucksack -> rucksack.toCharArray().toSet() }
-        val sharedItem = rucksackSets.reduce { acc, sack -> acc.intersect(sack) }.first()
+    val prioritySum: Int = rucksacks.chunked(3).sumOf { elfGroup ->
+        val rucksackSets = elfGroup.map { rucksack -> rucksack.toSet() }
+        val sharedItem: Char = rucksackSets.reduce { acc, sack -> acc.intersect(sack) }.first()
 
         sharedItem.itemPriority()
     }
