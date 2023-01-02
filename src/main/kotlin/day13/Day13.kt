167002b551367e5b242393eaaa2dc3b1ba7a9ec2
Andreas Lundin <andreaslundin47@gmail.com>
2023-01-03 00:06:56 +0100

    Days 13 & 25. Improved based on others' solutions

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -1,102 +1,78 @@
 package day13
 
-data class Signal(val signal: String): Comparable<Signal> {
+// Thanks to Todd Ginsberg for inspiration on improvements!
 
-    private val tokenized: List<String> = tokenize(signal)
 
-    private fun tokenize(signal: String): List<String> {
-        var remaining = signal
-        val tokens = mutableListOf<String>()
+sealed class Packet: Comparable<Packet> {
 
-        while (remaining.isNotEmpty()) {
+    companion object {
+        fun fromString(input: Iterator<String>): Packet {
+            val packets = mutableListOf<Packet>()
+            while (input.hasNext()) {
 
-            when (remaining.first()) {
-                '[', ']' -> {
-                    tokens += remaining.first().toString()
-                    remaining = remaining.drop(1)
+                when (val symbol = input.next()) {
+                    "]" -> return ListPacket(packets)
+                    "[" -> packets.add( fromString(input) )
+                    else -> packets.add( IntPacket(symbol.toInt()) )
                 }
-
-                ',' -> remaining = remaining.drop(1)
-
-                in '0'..'9' -> {
-                    tokens += remaining.takeWhile { it.isDigit() }
-                    remaining = remaining.dropWhile { it.isDigit() }
-                }
-
-                else -> error("Unexpected character while parsing signal representation")
             }
-        }
-        return tokens
-    }
 
-    override fun compareTo(other: Signal): Int {
-        return compare(this.tokenized, other.tokenized)
-    }
-
-
-    private fun compare(list1: List<String>, list2: List<String>): Int {
-
-        fun String.isNumber() = this !in setOf("[", "]")
-
-        fun List<String>.makeHeadList() = listOf("[", this.first(), "]") + this.drop(1)
-
-        fun compareRec(l1: List<String>, l2: List<String>): Int {
-            if (l1.isEmpty() && l2.isNotEmpty())
-                return -1
-
-            else if (l1.isNotEmpty() && l2.isEmpty())
-                return 1
-
-            else if (l1.first() == l2.first())
-                return compareRec(l1.drop(1), l2.drop(1))
-
-            else if (l1.first() == "]" && l2.first() != "]")
-                return -1
-
-            else if (l2.first() == "]" && l1.first() != "]")
-                return 1
-
-            else if (l1.first().isNumber() && l2.first() == "[")
-                return compareRec(l1.makeHeadList(), l2)
-
-            else if (l1.first() == "[" && l2.first().isNumber())
-                return compareRec(l1, l2.makeHeadList())
-
-            else if (l1.first().toInt() < l2.first().toInt())
-                return -1
-
-            else if (l1.first().toInt() > l2.first().toInt())
-                return 1
-
-            else
-                error("Unexpected list pair in comparison")
+            return ListPacket(packets)
         }
 
-        return compareRec(list1, list2)
+        fun fromString(input: String): Packet {
+            val regex = """((?<=[\[,\]])|(?=[\[,\]]))""".toRegex()
+            // With this regex we can split the string at all places that come before or
+            // after any of the three symbols: '['   ']'   ','
+            val tokens = input.split(regex).filter { it != "," }.filter { it.isNotBlank() }
+            return fromString(tokens.iterator())
+        }
+    }
+}
+
+class IntPacket(val value: Int): Packet() {
+
+    fun asList(): ListPacket = ListPacket(listOf(this))
+
+    override fun compareTo(other: Packet): Int {
+        return when (other) {
+            is IntPacket -> value.compareTo(other.value)
+            is ListPacket -> asList().compareTo(other)
+        }
+    }
+}
+
+class ListPacket(val packets: List<Packet>): Packet() {
+
+    override fun compareTo(other: Packet): Int {
+        return when (other) {
+            is IntPacket -> this.compareTo(other.asList())
+            is ListPacket -> packets.zip(other.packets)
+                .map { (p1, p2) -> p1.compareTo(p2) }
+                .firstOrNull { it != 0 } ?: packets.size.compareTo(other.packets.size)
+
+        }
     }
 }
 
 
-val signalPairs: List<Pair<Signal,Signal>> = java.io.File("src/main/resources/day13.txt")
-    .readText()
-    .trim()
-    .split("\n\n")
-    .map { block -> block.lines().let { (l1, l2) -> Signal(l1) to Signal(l2) }
-}
+val puzzleInput = java.io.File("src/main/resources/day13.txt").readLines()
+val packets = puzzleInput.filter { it.isNotBlank() }.map { Packet.fromString(it) }
+
 
 fun solvePartOne() {
-    val sum = signalPairs.mapIndexed { i, signals ->
-        signals.let { (s1, s2) -> if (s1 < s2) i+1 else 0 }
+    val sum = packets.chunked(2).mapIndexed { index, pair ->
+        if (pair[0] < pair[1]) index + 1 else 0
     }.sum()
     println("Part 1. Sum = $sum")
 }
 
 fun solvePartTwo() {
-    val divs = listOf( Signal("[[2]]"), Signal("[[6]]") )
-    val signalsAndDivs = divs + signalPairs.flatMap { it.toList() }
-    val sortedSignals = signalsAndDivs.sorted()
-    val prod = divs.map { sortedSignals.indexOf(it) + 1 }.reduce(Int::times)
-    println("Part 2. Product = $prod")
+    val dividerPacketOne = Packet.fromString("[[2]]")
+    val dividerPacketTwo = Packet.fromString("[[6]]")
+    val sorted = (packets + dividerPacketOne + dividerPacketTwo).sorted()
+    val key = (sorted.indexOf(dividerPacketOne) + 1) * (sorted.indexOf(dividerPacketTwo) + 1)
+    println("Part 2. Product = $key")
 }
 
 fun main() {
--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -1,69 +1,20 @@
 package day25
 
-import kotlin.math.pow
+// Thanks to Todd Ginsberg for inspiration on improvements!
+
+fun Long.toSNAFU() =
+        generateSequence(this) { (it + 2) / 5 }
+            .takeWhile { it > 0 }
+            .map { "012=-"[it.mod(5)] }
+            .joinToString("")
+            .reversed()
+
+
+fun String.toLong() = fold(0L) { acc, digit -> acc * 5 + "=-012".indexOf(digit) - 2 }
 
 val puzzleInput = java.io.File("src/main/resources/day25.txt").readLines()
 
-
-class Decimal(private val value: Long) {
-    override fun toString() = value.toString()
-    operator fun plus(other: Decimal) = Decimal(value + other.value)
-
-    fun toSNAFU(): SNAFU {
-        var snafu = ""
-        var remaining = value
-
-        while (remaining > 0L) {
-
-            val (symbol, delta) =
-                when (remaining.mod(5)) {
-                    0 -> '0' to 0
-                    1 -> '1' to 1
-                    2 -> '2' to 2
-                    3 -> '=' to -2
-                    4 -> '-' to -1
-                    else -> error("Strange SNAFU string representation")
-                }
-
-            snafu = symbol + snafu
-            remaining = (remaining - delta) / 5
-        }
-
-        return SNAFU(snafu)
-    }
-}
-
-
-class SNAFU(private val value: String) {
-    override fun toString() = value
-
-    fun toDecimal(): Decimal {
-        val decimalValue =
-            value.reversed().mapIndexed { index, symbol ->
-                5.0.pow(index) * when (symbol) {
-                    '0' -> 0
-                    '1' -> 1
-                    '2' -> 2
-                    '=' -> -2
-                    '-' -> -1
-                    else -> error("Strange digit")
-                }
-            }.sum().toLong()
-
-        return Decimal(decimalValue)
-    }
-}
-
-
-fun solvePartOne() {
-    val snafuList = puzzleInput.map { snafuString -> SNAFU(snafuString) }
-    val decimalList = snafuList.map { snafu -> snafu.toDecimal() }
-    val decimalSum = decimalList.fold(Decimal(0)) { acc, num -> acc + num }
-    val snafuSum = decimalSum.toSNAFU()
-
-    println("Part 1. SNAFU sum = $snafuSum")
-}
-
 fun main() {
-    solvePartOne()
+    val snafuSum = puzzleInput.sumOf { it.toLong() }.toSNAFU()
+    println("Part 1. SNAFU sum = $snafuSum")
 }
