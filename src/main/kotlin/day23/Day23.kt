85dde10a4e40309ea8020942e4382ea841e6d887
Andreas Lundin <andreaslundin47@gmail.com>
2023-04-03 01:05:07 +0200

    Day 23, improved by reading other's solutions

--- src/main/kotlin/day23/Day23.kt
+++ src/main/kotlin/day23/Day23.kt
@@ -2,130 +2,114 @@ package day23
 
 val input = java.io.File("src/main/resources/day23.txt").readText().trim()
 
-class Rectangle(val xRange: IntRange, val yRange: IntRange) {
-    fun area() = xRange.count() * yRange.count()
+fun main() {
+    println("Part 1. Empty ground tiles: ${Day23(input).solvePartOne()}")
+    println("Part 2. Time of equilibrium: ${Day23(input).solvePartTwo()}")
 }
 
-data class Pos(val x: Int, val y: Int) {
-    fun north() = Pos(x, y-1)
-    fun south() = Pos(x, y+1)
-    fun west() = Pos(x-1, y)
-    fun east() = Pos(x+1, y)
+data class Point2D(val x: Int, val y: Int) {
 
-    fun northern() = listOf(-1, 0, 1).map { Pos(x + it, y - 1) }
-    fun southern() = listOf(-1, 0, 1).map { Pos(x + it, y + 1) }
-    fun western() = listOf(-1, 0, 1).map { Pos(x - 1, y + it) }
-    fun eastern() = listOf(-1, 0, 1).map { Pos(x + 1, y + it) }
+    infix operator fun plus(other: Point2D) = Point2D(x + other.x, y + other.y)
+
+    fun neighbours8(): Set<Point2D>
+    {
+        val x = this.x
+        val y = this.y
+
+        return setOf(
+            Point2D(x-1,y+1),
+            Point2D(x, y+1),
+            Point2D(x+1,y+1),
+            Point2D(x-1, y),
+            Point2D(x+1,y),
+            Point2D(x-1, y-1),
+            Point2D(x, y-1),
+            Point2D(x+1,y-1)
+        )
+    }
 }
 
-class ElfTracker(private val elfPositions: Set<Pos>, val turn: Int = 0) {
+class Day23(val input: String) {
 
-    companion object {
-        fun fromString(text: String): ElfTracker {
-            val elves = text.lines().flatMapIndexed { y, line ->
-                line.mapIndexedNotNull { x, entry ->
-                    if (entry == '#') Pos(x, y) else null
-                }
-            }.toSet()
-            return ElfTracker(elves, turn = 0)
-        }
+    private val startPositions: Set<Point2D> = parseInput(input)
+    private val nextTurnOffsets: List<List<Point2D>> = createOffsets()
+
+    fun solvePartOne(): Int {
+        var positions = startPositions
+        repeat(10) {index -> positions = positions.playRound(index) }
+        return positions.rectangleArea() - positions.size
     }
 
-    infix fun samePositions(other: ElfTracker) = elfPositions == other.elfPositions
+    fun solvePartTwo(): Int {
+        var roundNumber = 0
+        var thisTurn = startPositions
+        do {
+            val previousTurn = thisTurn
+            thisTurn = thisTurn.playRound(roundNumber++)
+        } while (thisTurn != previousTurn)
 
-    private fun smallestBoundingRectangle(): Rectangle {
-        val xRange = elfPositions.minOf { it.x } .. elfPositions.maxOf { it.x }
-        val yRange = elfPositions.minOf { it.y } .. elfPositions.maxOf { it.y }
-        return Rectangle(xRange, yRange)
+        return roundNumber
     }
 
-    fun countOpenGround() = smallestBoundingRectangle().area() - elfPositions.size
+    private fun createOffsets(): List<List<Point2D>> =
+        listOf(
+            listOf(Point2D(0, -1), Point2D(-1, -1), Point2D(1, -1)),  // North
+            listOf(Point2D(0, 1), Point2D(-1, 1), Point2D(1, 1)),     // South
+            listOf(Point2D(-1, 0), Point2D(-1, 1), Point2D(-1, -1)),  // West
+            listOf(Point2D(1, 0), Point2D(1,  1), Point2D(1, -1))     // East
+        )
 
-    fun draw() {
-        val rec = smallestBoundingRectangle()
-        println()
-        for (y in rec.yRange) {
-            for (x in rec.xRange) {
-                when ( Pos(x,y) ) {
-                    in elfPositions -> print("#")
-                    else -> print(".")
-                }
+
+    private fun parseInput(input: String): Set<Point2D> =
+        input.lines().flatMapIndexed { y, row ->
+            row.mapIndexedNotNull { x, sym ->
+                if (sym == '#') Point2D(x, y) else null
             }
-            println()
-        }
-        println()
-    }
-
-    private fun destinationOfElf(pos: Pos): Pos {
-        val neighbours = (pos.northern() + pos.southern() + pos.western() + pos.eastern()).toSet()
-
-        // Don't move if no other elf is within the eight adjacent positions
-        if (neighbours.all { it !in elfPositions }) {
-            return pos
-        }
-
-        // Otherwise try directions in the given order, and pick the first that is viable
-        val startIndex = turn % 4
-
-        for (i in 0..3) {
-            when ( (startIndex + i) % 4 ) {
-                0 -> if (!pos.northern().any { it in elfPositions }) return pos.north()
-                1 -> if (!pos.southern().any { it in elfPositions }) return pos.south()
-                2 -> if (!pos.western().any { it in elfPositions }) return pos.west()
-                3 -> if (!pos.eastern().any { it in elfPositions }) return pos.east()
-                else -> error("Bad index!")
-            }
-        }
-
-        // Stay if no direction was viable
-        return pos
-    }
-
-    fun next(): ElfTracker {
-        // Determine where each elf would like to go
-        val elvesWithTargets = elfPositions.map { it to destinationOfElf(it) }
-
-        // Find all locations where more than one elf want to go
-        val targets = mutableSetOf<Pos>()
-        val forbidden = mutableSetOf<Pos>()
-
-        elvesWithTargets.forEach { (_, target) ->
-            if (target in targets) {
-                forbidden += target
-            }
-            targets += target
-        }
-
-        // Update all positions and return the new state.
-        // Only elves with unique destinations will move
-        val nextPositions = elvesWithTargets.map { (elf, target) ->
-            if (target !in forbidden) target else elf
         }.toSet()
 
-        return ElfTracker(nextPositions, turn = turn + 1)
+
+    private fun Set<Point2D>.rectangleArea(): Int {
+        val dx = this.maxOf { it.x } - this.minOf { it.x } + 1
+        val dy = this.maxOf { it.y } - this.minOf { it.y } + 1
+
+        return dx * dy
     }
-    
+
+    private fun Set<Point2D>.playRound(roundIndex: Int = 0): Set<Point2D> {
+
+        // Copy the current positions
+        val nextPositions = this.toMutableSet()
+
+        // An elf wants to move if they have at least one neighbour
+        val wantsToMove: List<Point2D> = this.filter { elf -> elf.neighbours8().any { it in this } }
+
+        // For each aspiring mover, find if their valid move, if any
+        val moversAndDestinations: Map<Point2D, Point2D> =
+            wantsToMove
+                .mapNotNull { elf ->
+                    nextTurnOffsets.indices.map { dirIndex -> nextTurnOffsets[(dirIndex + roundIndex) % 4] }
+                        .firstNotNullOfOrNull { offsets ->
+                            if (offsets.none {offset -> (elf + offset) in this})
+                                elf to (elf + offsets[0])
+                            else null
+                        }
+                }.toMap()
+
+        // Only unique destinations are valid destinations
+        val safeDestinations: Set<Point2D> =
+            moversAndDestinations.values.groupingBy { it }.eachCount().filter { it.value == 1 }.keys
+
+        // Move all the elves that are able to do so
+        moversAndDestinations
+            .filter { (_,destination) -> destination in safeDestinations }
+            .forEach { (elf, destination) ->
+                nextPositions.remove(elf)
+                nextPositions.add(destination)
+            }
+
+        return nextPositions
+   }
+
 }
 
 
-fun solvePartOne() {
-
-    var elves = ElfTracker.fromString(input)
-    repeat(10) { elves = elves.next() }
-    println("Part 1. Ground = ${elves.countOpenGround()}")
-}
-
-fun solvePartTwo() {
-    var elfTracker = ElfTracker.fromString(input)
-    while (true) {
-        val updatedElfTracker = elfTracker.next()
-        if (elfTracker samePositions updatedElfTracker) break
-        elfTracker = updatedElfTracker
-    }
-    println("Part 2. No change in round ${elfTracker.turn + 1}")
-}
-
-fun main() {
-    solvePartOne()
-    solvePartTwo()
-}
