d4c22ede72ab8b9fc76607e8096341f01b9c1543
Andreas Lundin <andreaslundin47@gmail.com>
2023-12-01 22:24:34 +0100

    Day 1. Kotlin

--- Kotlin/src/main/kotlin/Day01.kt
+++ Kotlin/src/main/kotlin/Day01.kt
@@ -3,8 +3,42 @@ var rawInput = java.io.File("src/main/resources/input-01.txt").readText().trim()
 
 class Day01(input: String) {
 
+    private val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    private val lines = input.lines()
+
+    private fun String.toValueString(): String {
+        return (digitWords.indexOf(this) + 1).toString()
+    }
+
+    private fun String.substituteWords(): String =
+        mapIndexed { i, c ->
+            when (c.isDigit()) {
+                true -> c
+                false -> {
+                    val wordMatch = digitWords.firstOrNull { substring(i).startsWith(it) }
+                    wordMatch?.toValueString() ?: ""
+                }
+            }
+        }
+        .joinToString()
+
+
+    private fun String.calibrationValue(): Int =
+        "${first{ it.isDigit() }}${last { it.isDigit() }}".toInt()
+
+    fun solvePartOne() {
+        val sum = lines.sumOf { line -> line.calibrationValue() }
+        println("Part 1. Sum = $sum")
+    }
+
+    fun solvePartTwo() {
+        val sum = lines.sumOf { line -> line.substituteWords().calibrationValue() }
+        println("Part 2. Sum = $sum")
+    }
 }
 
 fun main() {
-    println(rawInput)
+    val d = Day01(rawInput)
+    d.solvePartOne()
+    d.solvePartTwo()
 }
