0b37a3731ae4cc3a0aaed56d5a4abe7f08ae9618
Andreas Lundin <andreaslundin47@gmail.com>
2023-12-05 01:16:19 +0100

    Day 4. Rust - cleaned up

--- Rust/day-04/src/main.rs
+++ Rust/day-04/src/main.rs
@@ -1,79 +1,65 @@
-use std::str::FromStr;
-use regex::Regex;
+use std::{str::FromStr, collections::HashSet};
 
 struct Card {
-    winning_numbers: Vec<u32>,
-    numbers: Vec<u32>,
+    winning_numbers: HashSet<usize>,
+    our_numbers: HashSet<usize>,
 }
 
 impl FromStr for Card {
     type Err = String;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(": ").collect();
-        let num_parts: Vec<&str> = parts[1].split(" | ").collect();
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (_, numbers) = line.split_once(":").unwrap();
+        let (winning_numbers, our_numbers) = numbers.split_once("|").unwrap();
 
-        let pattern: Regex = Regex::new(r"\d+").expect("Should be a valid regexp");
-
-        let winning_numbers: Vec<u32>= pattern
-            .find_iter(num_parts[0])
-            .map(|c| c.as_str().parse().expect("Should parse an u32"))
+        let winning_numbers: HashSet<usize> = winning_numbers
+            .split_whitespace()
+            .filter_map(|n| n.parse().ok())
             .collect();
 
-        let numbers: Vec<u32>= pattern
-            .find_iter(num_parts[1])
-            .map(|c| c.as_str().parse().expect("Should parse an u32"))
+        let our_numbers: HashSet<usize> = our_numbers
+            .split_whitespace()
+            .filter_map(|n| n.parse().ok())
             .collect();
 
-        Ok( Card { winning_numbers, numbers } )
+        Ok( Card { winning_numbers, our_numbers } )
     }
 }
 
 impl Card {
-    fn count_winning_numbers(&self) -> usize {
-        let matches: usize = self.winning_numbers
-            .iter()
-            .filter(|w| self.numbers.contains(w))
-            .count();
-        
-        matches
-    }
-    fn score(&self) -> u32 {
-        let wins: usize = self.count_winning_numbers();
+    fn correct_numbers_count(&self) -> usize {
+        self.winning_numbers.intersection(&self.our_numbers).count()
+   }
+    fn score(&self) -> usize {
+        let correct = self.correct_numbers_count();
 
-        match wins {
+        match correct {
             0 => 0,
-            _ => 2u32.pow(wins as u32 - 1)
+            _ => 2usize.pow(correct as u32 - 1)
         }
     }
 }
 
 fn solve_part_one(cards: &Vec<Card>) {
-    let sum: u32 = cards.iter().map(|c| c.score()).sum();
+    let sum: usize = cards.iter().map(|card| card.score()).sum();
     println!("Part 1. Sum = {sum}");
 }
 
 fn solve_part_two(cards: &Vec<Card>) {
-    let win_counts: Vec<usize> = cards
+    let corrects_on_cards: Vec<usize> = cards
         .iter()
-        .map(|c| c.count_winning_numbers())
+        .map(|card| card.correct_numbers_count())
         .collect();
 
-    let mut card_counts: Vec<u32> = vec![1 ; cards.len()];
+    let mut card_amounts: Vec<usize> = vec![1 ; cards.len()];
 
-    for i in 0..card_counts.len() {
-        let card_wins: usize = win_counts[i];
-
-        for j in 1..=card_wins {
-            if (i+j) < cards.len() {
-                card_counts[i+j] = card_counts[i+j] + card_counts[i]
-            }
+    for (card_number, &correct_numbers_on_card) in corrects_on_cards.iter().enumerate() {
+        for i in 1..=correct_numbers_on_card {
+            card_amounts[card_number + i] += card_amounts[card_number];
         }
     }
 
-    let total_cards: u32 = card_counts.iter().sum();   
-
-    println!("Part 2. Number of cards: {total_cards}");
+    println!("Part 2. Number of cards: {}", card_amounts.iter().sum::<usize>());
 }
 
 
@@ -81,9 +67,7 @@ fn main() {
     let raw_input = include_str!("../input.txt").trim();
 
     let cards: Vec<Card> = raw_input
-        .lines()
-        .map(|line| Card::from_str(line).expect("Should parse a card"))
-        .collect();
+        .lines().map(|line| Card::from_str(line).unwrap()).collect();
 
     solve_part_one(&cards);
     solve_part_two(&cards);
