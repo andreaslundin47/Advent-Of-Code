c00052c90583fcbd77c8adb8b29b418fb66e9f18
Andreas Lundin <andreaslundin47@gmail.com>
2023-12-09 17:44:27 +0100

    Day 6. Changed from brute force to solving quadratic equation.

--- Rust/day-06/src/main.rs
+++ Rust/day-06/src/main.rs
@@ -6,75 +6,70 @@ fn main() {
     solve_part_one(&durations, &records);
     solve_part_two(&durations, &records);
 }
+// ----------------------------------------------------------------------------
+fn solve_part_one(durations: &Vec<usize>, records: &Vec<usize>) {
+    let possibilities_per_race: Vec<usize> = durations
+        .iter()
+        .zip(records.iter())
+        .map(|(&duration, &record_distance)| count_valid_options(duration, record_distance))
+        .collect::<Vec<usize>>();
 
-fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let product: usize = possibilities_per_race.iter().product();
+    println!("Part 1. Product is {product}");
+}
+// ----------------------------------------------------------------------------
+fn solve_part_two(durations: &Vec<usize>, records: &Vec<usize>) {
+    let duration: usize = durations
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("Should concatenate the numbers");
+
+    let record_distance: usize = records
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .expect("Should concatenate the numbers");
+
+    let winning_ways: usize = count_valid_options(duration, record_distance);
+    println!("Part 2. Ways to win {winning_ways}");
+}
+// ----------------------------------------------------------------------------
+fn count_valid_options(duration: usize, record_distance: usize) -> usize {
+    let (start, end) = solve_for_interval(duration, record_distance);
+    end - start + 1
+}
+
+//   t * (d - t) = r => dt - t^2 = r => t^2 - dt + r = 0
+//   => t = d/2 +- sqrt(d^2/4 - r)
+fn solve_for_interval(duration: usize, record: usize) -> (usize, usize) {
+    let (d, r) = (duration as f64, record as f64);
+    let left = d / 2.0 - (d.powi(2) / 4.0 - r).sqrt();
+    let right = d / 2.0 + (d.powi(2) / 4.0 - r).sqrt();
+
+    (left.ceil() as usize, right.floor() as usize)
+}
+// ----------------------------------------------------------------------------
+fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
     let (durations, records) = input
         .lines()
         .map(|line| {
-            line
-                .split_once(":")
+            line.split_once(":")
                 .map(|(_label, numbers_part)| {
-                    numbers_part
-                        .split_whitespace()
-                        .map(|number| {
-                            number
-                                .parse::<u32>()
-                                .expect("Should parse a single integer")
-                        })
+                    numbers_part.split_whitespace().map(|number| {
+                        number
+                            .parse::<usize>()
+                            .expect("Should parse a single integer")
+                    })
                 })
                 .expect("Should have a parsed list of integers")
-                .collect::<Vec<u32>>()
+                .collect::<Vec<usize>>()
         })
-        .collect_tuple::<(Vec<u32>, Vec<u32>)>()
+        .collect_tuple::<(Vec<usize>, Vec<usize>)>()
         .expect("Should have collected a tuple of integer vectors");
 
     (durations, records)
 }
-
-fn solve_part_one(durations: &Vec<u32>, records: &Vec<u32>) {
-    let possibilities_per_race: Vec<usize> = durations
-            .iter()
-            .zip(records.iter())
-            .map(|(&duration, &record_distance)| {
-                (0..duration).filter(|t| {
-                    let velocity = t;
-                    let moving_duration = duration - t;
-                    let distance = velocity * moving_duration;
-
-                    distance > record_distance
-                })
-                .count()
-            })
-            .collect::<Vec<usize>>();
-
-        let product: usize = possibilities_per_race.iter().product();
-        println!("Part 1. Product is {product}");
-}
-
-fn solve_part_two(durations: &Vec<u32>, records: &Vec<u32>) {
-    let duration: usize = durations
-            .iter()
-            .map(|n| n.to_string())
-            .collect::<String>()
-            .parse::<usize>()
-            .expect("Should concatenate the numbers");
-
-    let record_distance: usize = records
-            .iter()
-            .map(|n| n.to_string())
-            .collect::<String>()
-            .parse::<usize>()
-            .expect("Should concatenate the numbers");
-    
-    let winning_ways: usize = (0..duration)
-            .filter(|t| {
-                let velocity = t;
-                let moving_duration = duration - t;
-                let distance = velocity * moving_duration;
-                
-                distance > record_distance
-            })
-            .count();
-    
-    println!("Part 2. Ways to win {winning_ways}");
-}
+// ----------------------------------------------------------------------------
